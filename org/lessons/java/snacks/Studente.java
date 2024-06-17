593be1b06b16c33c2358ca16015acde4a96bce19
s-motto <brinamotto@gmail.com>
2024-06-17 21:49:22 +0200

    added snack 1

---
+++ org/lessons/java/snacks/Studente.java
@@ -0,0 +1,31 @@
+package org.lessons.java.snacks;
+//Snack 1: Studente
+//Scrivi una classe Studente con i seguenti attributi: nome, cognome, età. 
+//Implementa un costruttore che accetta tre parametri per inizializzare questi attributi. 
+//Aggiungi un metodo che restituisca una stringa concatenata dei valori (es. Anselmo Bitta, 24 anni).
+public class Studente {
+
+	private String nome;
+	private String cognome;
+	private int eta;
+	
+	
+	public Studente (String nome, String cognome, int eta) {
+		this.nome=nome;
+		this.cognome=cognome;
+		this.eta=eta;
+	}
+	
+	public String getInfoStudente() {
+		return this.nome + " "+ this.cognome +", "+ this.eta+" anni.";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Studente studenteUno= new Studente("Khadim", "Diop", 10);
+    Studente studenteDue= new Studente("Sabrina", "Motto", 33);
+    
+    System.out.println(studenteUno.getInfoStudente());
+    System.out.println(studenteDue.getInfoStudente());
+	}
+
+}
